b9a91094d4143c168c54b728721fc727eab3cc94
Michele Cianni <michele.cianni@studenti.unicam.it>
2025-05-09 18:01:38 +0200

    Update report_writer.h

--- include/report_writer.h
+++ include/report_writer.h
@@ -6,11 +6,14 @@
 
 /**
  * Print the formatted report to stdout.
- * @param dirs    Array of directory statistics, terminated by an entry with path=NULL.
- * @param exts    Array of extension statistics, terminated by an entry with ext=NULL.
- * @param avg     Average file size in bytes.
+ * @param dirs      Array of directory statistics.
+ * @param dir_count Number of elements in the dirs array.
+ * @param exts      Array of extension statistics.
+ * @param ext_count Number of elements in the exts array.
+ * @param avg       Average file size in bytes.
  * @return 0 on success, -1 on failure (with errno set).
  */
-int du_report_print(const DirStatsArr *dirs, const ExtStatsArr *exts, double avg);
+int du_report_print(const DirStatsArr *dirs, size_t dir_count, 
+                    const ExtStatsArr *exts, size_t ext_count, double avg);
 
 #endif //REPORT_WRITER_H
